a314a02b750e8b965921e1a308fa98e501ff1156
VVVladyslav <vladoszhar@gmail.com>
2023-09-23 21:56:47 +0300

    lasst

--- src/Result.java
+++ src/Result.java
@@ -1,21 +1,15 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Result {
 
     public static String PrintName(List<String> names) {
+        return IntStream.range(0, names.size())
+                .filter(i -> i % 2 == 0)
+                .mapToObj(i -> (i + 1) + ". " + names.get(i))
+                .collect(Collectors.joining(", "));
 
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < names.size(); i++) {
-
-            if (i % 2 == 0) {
-                if (result.length() > 0) {
-                    result.append(", ");
-                }
-                result.append((i + 1) + ". " + names.get(i));
-            }
-        }
-        return result.toString();
     }
 }
-
